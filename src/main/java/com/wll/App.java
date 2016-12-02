d46d5c53b823ec2bd63c1f961fff37f1132dfd2a
joinss <1814162565@qq.com>
2016-12-02 15:54:15 +0800

    Update App.java
    test pull joinss

--- src/main/java/com/wll/App.java
+++ src/main/java/com/wll/App.java
@@ -11,5 +11,6 @@ public class App
     	System.out.println( "Hello World!" );
     	System.out.println("这是wll-joins提交的数据");
     	System.out.println("这是joinss提交的数据");
+        System.out.println("测试pull");
     }
 }
