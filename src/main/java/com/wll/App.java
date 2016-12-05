8de2bb2637d9e283d521301ace918db07aa712eb
joinss <1814162565@qq.com>
2016-12-05 17:28:29 +0800

    Update App.java
    git

--- src/main/java/com/wll/App.java
+++ src/main/java/com/wll/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-    	System.out.println( "Hello  World!" );
+    	System.out.println( "Hello  git!" );
     	System.out.println("这是wll-joins提交的数据");
     	System.out.println("这是joinss提交的数据");
         System.out.println("测试pull");
